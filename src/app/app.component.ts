69b8a9efae511a3d20797126788e4b818ecdd668
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-12-05 22:55:02 -0500

    not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1>
+  <ul>
+    <li *ngFor="let food of foods">
+  <h2> {{food.name}}</h2>
+  <p> {{food.brand}}<p>
+  <p> {{food.price | currency: 'US':true}}<p>
+</li>
+</ul>`
 })
+
 export class AppComponent {
-  title = 'myfirstapp';
-}
+    title = 'Burgers n Fries'
+    foods = [{
+      "id": 1,
+      "name": "Cheese Burger",
+      "brand": "Mcdonalds",
+      "price": "$4.00"
+    },
+    {
+      "id": 2,
+      "name": "Salty Fries",
+      "brand": "Burger king",
+      "price": "$2.00"
+    },
+      {
+        "id": 3 ,
+        "name": "Cold Soda",
+        "brand": "CocaCola",
+        "price": "$2.50"
+    }];
+  }
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
-import { AppModule } from './app/app.module';
 
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1>
+  <ul>
+    <li *ngFor="let food of foods">
+  <h2> {{food.name}}</h2>
+  <p> {{food.brand}}<p>
+  <p> {{food.price | currency: 'US':true}}<p>
+</li>
+</ul>`
+})
+
+
+@NgModule({
+  declarations: [ AppComponent],
+  imports: [ BrowserModule ],
+  bootstrap: [AppComponent]
+})
+
+ class AppModule{}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
